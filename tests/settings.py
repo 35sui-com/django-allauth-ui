0acb23f8df0c38a7b6bdfc6d691df37c51e4dae5
sigezhao <1013885933@qq.com>
2024-05-18 22:23:43 +0800

    先去掉第三方登录，然后增加密码强度校验，新增QQ邮箱发邮件能力

--- tests/settings.py
+++ tests/settings.py
@@ -5,6 +5,8 @@ from django.utils.translation import gettext_lazy as _
 DEBUG = True
 USE_TZ = True
 
+import os
+
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "very-secret"
 
@@ -56,6 +58,21 @@ MIDDLEWARE = [
     "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
@@ -85,8 +102,8 @@ AUTHENTICATION_BACKENDS = [
     "allauth.account.auth_backends.AuthenticationBackend",
 ]
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
-INTERNAL_IPS = ["127.0.0.1"]
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+# INTERNAL_IPS = ["127.0.0.1"]
 ALLOWED_HOSTS = ["*"]
 SITE_ID = 1
 STATIC_URL = "/static/"
@@ -99,3 +116,14 @@ ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_EMAIL_VERIFICATION = "mandatory"
 ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_LOGIN_ATTEMPTS_LIMIT = 1000
+
+# Email settings
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.qq.com')
+EMAIL_PORT = os.getenv('EMAIL_PORT', 465)
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'ai_future@foxmail.com')
+#EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '1371605683@qq.com')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'lhtstalemirpjddj')
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', False)
+EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', True)
+DEFAULT_FROM_EMAIL = os.getenv('EMAIL_FROM', 'ai_future@foxmail.com')
